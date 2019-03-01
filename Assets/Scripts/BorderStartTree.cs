e218434b0c1ca77d2949a5095c0f8625e902a8b4
Jankymage <36413963+Jankymage@users.noreply.github.com>
2019-03-01 18:19:42 +0100

    Initial commit

---
+++ Assets/Scripts/BorderStartTree.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//check if clicked
+//checked locatie van speler
+//animate player from A tot B or B to A 
+//past de locatie van de speler aan in info
+
+
+public class BorderStartTree : MonoBehaviour {
+
+
+	//voor de animatie/verplaatsing
+	public Animator playerMovement;
+
+	//word gebruikt om locatie an de speler bij te houden.
+	public PlayerInfo PlayerInfo;
+
+	void Update(){
+		
+	}
+
+	//checked of er op het object geklikt word
+	void OnMouseDown(){
+		
+		//checked of speler in start is
+		//zo ja, animeer naar tree
+		if(PlayerInfo.playerLocation == 1){
+			Debug.Log(PlayerInfo.playerLocation);
+			playerMovement.Play("StartToTree");
+			PlayerInfo.playerLocation = 2;
+		}	
+
+		//checked of speler in Tree is
+		//zo ja, animeer naar Start
+		else if(PlayerInfo.playerLocation == 2){
+			Debug.Log(PlayerInfo.playerLocation);
+			playerMovement.Play("TreeToStart");
+			PlayerInfo.playerLocation = 1;
+		}
+
+
+
+	}
+
+}
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//script voor het bijhouden van info van de player
+
+public class PlayerInfo : MonoBehaviour {
+
+	public int playerLocation = 1;
+
+}
