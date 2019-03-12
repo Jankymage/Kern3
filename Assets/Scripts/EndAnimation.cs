caa1a3b1cdc4d8eeed9f0d20feb6cd72bd8dfebe
Jankymage <36413963+Jankymage@users.noreply.github.com>
2019-03-12 21:06:43 +0100

    Movement veranderd
    nu aan de hand van buttens op scherm
    buttons deactiveren aan begin animatie en activeren aan eind animatie via event

---
+++ Assets/Scripts/EndAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAnimation : MonoBehaviour {
+
+	public GameObject objectLeft;
+	public GameObject objectRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//zet de knoppen aan begin animatie uit en aan eind weer aan
+	void ActivateButtons(){
+		if(objectLeft.activeSelf == true){
+			objectLeft.SetActive(false);
+			objectRight.SetActive(false);
+		}
+		else{
+			objectLeft.SetActive(true);
+			objectRight.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	//voor de knoppen
+	public Button buttonLeft;
+	public Button buttonRight;
+
+	//voor de animatie/verplaatsing
+	public Animator playerMovement;
+
+	//word gebruikt om locatie an de speler bij te houden.
+	public PlayerInfo PlayerInfo;
+
+	// Use this for initialization
+	void Start () {
+		//om de knopen te initialiseren
+		buttonLeft.onClick.AddListener(MoveLeft);
+		buttonRight.onClick.AddListener(MoveRight);
+	}
+
+
+//if clicked
+//check PlayerInfo.playerLocation
+//play right animation
+
+
+	void MoveLeft(){
+		if(PlayerInfo.playerLocation == 1){
+			playerMovement.Play("StartToTree");
+			PlayerInfo.playerLocation = 2;
+		}
+		
+		else if(PlayerInfo.playerLocation == 2){
+			playerMovement.Play("TreeToCommunication");
+			PlayerInfo.playerLocation = 3;
+		}
+
+		else if(PlayerInfo.playerLocation == 3){
+			playerMovement.Play("CommunicationToPollen");
+			PlayerInfo.playerLocation = 4;
+		}
+
+		else if(PlayerInfo.playerLocation == 4){
+			playerMovement.Play("PollenToSuckers");
+			PlayerInfo.playerLocation = 5;
+		}
+
+	}
+
+	void MoveRight(){
+
+		if(PlayerInfo.playerLocation == 5){
+			playerMovement.Play("SuckersToPollen");
+			PlayerInfo.playerLocation = 4;
+		}
+		
+		else if(PlayerInfo.playerLocation == 4){
+			playerMovement.Play("PollenToCommunication");
+			PlayerInfo.playerLocation = 3;
+		}
+
+		else if(PlayerInfo.playerLocation == 3){
+			playerMovement.Play("CommunicationToTree");
+			PlayerInfo.playerLocation = 2;
+		}
+
+		else if(PlayerInfo.playerLocation == 2){
+			playerMovement.Play("TreeToStart");
+			PlayerInfo.playerLocation = 1;
+		}
+
+	}
+
+}
